b1f2a57b50d34439ed8a5d791a16848809b6f755
ivanLFN <LFN.current@yandex.com>
2023-04-11 21:49:56 -0700

    added data from database to menu page

--- restarant/menu/urls.py
+++ restarant/menu/urls.py
@@ -1,7 +1,7 @@
-from django.views.generic import TemplateView
-from django.contrib import admin
+
 from django.urls import include, path
+from menu.views import menu_home
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='menu.html'), name='menu'),
+    path('', menu_home, name='menu_home'),
 ]
--- restarant/menu/views.py
+++ restarant/menu/views.py
@@ -7,5 +7,5 @@ def menu_home(request):
     # fast_food = MenuPosition.objects.filter(tag='fast-food')
     fast_food = MenuPosition.objects.filter(tag='fast-food')
     pizzas = MenuPosition.objects.filter(tag='pizza')
-    
-    return render(request, 'menu_list.html', {'fast_food': fast_food, 'pizzas': pizzas})
+
+    return render(request, 'menu.html', {'fast_food': fast_food, 'pizzas': pizzas})
